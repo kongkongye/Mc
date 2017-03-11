7382b14b7edf7d3e38e20c81d3c0ab593b1a0e16
kongkongye <kongkongye@163.com>
2017-03-11 16:53:43 +0800

    update

--- js/waterfall.js
+++ js/waterfall.js
@@ -15,7 +15,7 @@
     var demoContent = [
         {
             demo_link: 'http://cmd.kongkongye.com',
-            img_link: 'https://cmd.kongkongye.com/imgs/CmdProject.png',
+            img_link: '/imgs/CmdProject.png',
             code_link: 'http://cmd.kongkongye.com',
             title: '命令项目',
             core_tech: 'java',
